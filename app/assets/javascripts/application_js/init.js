d5bc341f74a71dfe4e9bd23bb4945fdaa63b47c6
Michał Pawelski <kontakt@proapi.eu>
2013-07-26 15:08:15 +0200

    adds cookieDirective.js and changes the deploy script

--- app/assets/javascripts/application_js/init.js
+++ app/assets/javascripts/application_js/init.js
@@ -46,6 +46,19 @@ $(function () {
     });
 });
 
+//$(function cookiesDirectiveScriptWrapper() {
+    // this block of code was initially before closing body tag
+    // Cookie creating scripts etc here....
+    // cdScriptAppend('google.js', 'head');
+//});
+
+$(function () {
+// The position of the disclosure ('top' or 'bottom')
+// Number of times to display disclosure. Enter 0 to show it forever!!!!
+// The URI of your privacy policy
+    cookiesDirective('bottom', 10, 'polityka-prywatnosci');
+});
+
 function initTooltip() {
     $(function () {
         $('.help').tooltip({
